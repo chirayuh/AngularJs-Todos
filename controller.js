211cb2b746898dbeb5e3d1413c42cfb4e8e40968
Chirayu H <chirayuh69@gmail.com>
2021-09-11 09:01:29 +0530

    Adding buying book list.
    To add another listing module called booking listing.

--- controller.js
+++ controller.js
@@ -1,7 +1,13 @@
 app.controller('controller', ['$scope', function($scope) {
-    $scope.list = ["Go for a walk","Learn Fishing","Go to sleep"];
-    $scope.addItem = function() {
-        $scope.list.push($scope.addTodo);
-        $scope.addTodo = '';
+    $scope.todo = {
+        list : ["Go for a walk","Learn Fishing","Go to sleep"]
+    }
+
+    $scope.books = {
+        list : []
+    }
+
+    $scope.addItem = function(itemList, item) {
+        itemList.push(item);
     }
 }])
