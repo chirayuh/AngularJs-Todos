a9a7a078f313bd854415ed288064fbaed24c8933
Chirayu H <chirayuh69@gmail.com>
2021-09-11 10:22:42 +0530

    Use of http service to fetch googleBookApi.
    We used http service to fetch google api  book and render the data in the angular applciation.

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,4 +1,4 @@
-app.controller('controller', ['$scope', function($scope) {
+app.controller('controller', ['$scope' ,'$http', function($scope , $http) {
     $scope.todo = {
         title: "Things I need to do:",
         list : ["Go for a walk","Learn Fishing","Go to sleep"]
@@ -9,7 +9,29 @@ app.controller('controller', ['$scope', function($scope) {
         list : []
     }
 
+    function hadOnlyNumber(item)
+    {
+        return /^[0-9]*$/.test(item);
+    }
+
     $scope.addItem = function(itemList, item) {
-        itemList.push(item);
+        // ISBN : 10 or 13 length and consist of only Numbers.
+        if( (item.length === 10 || item.length === 13) && hadOnlyNumber(item))
+        {
+            //Sample-ISBN: 0747532695
+            $http({
+                method: 'get', 
+                url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+ item
+            }).then(function (response) {
+                data = response.data;
+                itemList.push("Title: "+ data.items[0].volumeInfo.title +" \n Author(s): "+ data.items[0].volumeInfo.authors);
+            },function (error){
+                console.log(error, 'can not get data.');
+            });
+        }
+        else 
+        {
+            itemList.push(item);
+        }
     }
 }])
